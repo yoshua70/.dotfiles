cff446f3eccd737d7b6e860eed202e78c33e9e07
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-06-21 13:59:19 +0000

    feat: set nvimtree mapping
    Update the nvimtree toggle keybinding. Set it to <space+e>.

--- nvim/lua/mappings/init.lua
+++ nvim/lua/mappings/init.lua
@@ -1,5 +1,8 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', '<c-u>', ':NvimTreeToggle<CR>', opts)
+vim.g.mapleader = ' '
+
+--map('n', '<c-u>', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 
