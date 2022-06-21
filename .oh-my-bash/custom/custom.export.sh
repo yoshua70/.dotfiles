bf44bf873b4a98f6d70279a4f353c196a2ebce5c
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-06-21 14:05:44 +0000

    feat: add deno to path

--- .oh-my-bash/custom/custom.export.sh
+++ .oh-my-bash/custom/custom.export.sh
@@ -6,6 +6,9 @@ export HADOOP_HOME=~/hadoop/hadoop-3.3.0
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
 
+#Deno
+export DENO_INSTALL=/home/michee/.deno
+
 #PATH
-export PATH=$PATH
+export PATH=$PATH:$DENO_INSTALL/bin
 #export PATH=$PATH:$HADOOP_HOME/bin:$HOME/.local/bin
